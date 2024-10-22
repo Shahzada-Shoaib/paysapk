e728f499b301b4884555f3bc40632edd0cf3d849
Shahzada Salman <shahzada.salman@convertdigital.com.au>
2024-10-22 15:43:40 +0500

    preloading the video:

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -8,22 +8,16 @@ function Banner() {
       <div className="banner">
         <div className="px-4 lg:px-0">
           <h1 className="max-w-[883px] mx-auto text-center">
-            {" "}
             EMPOWER YOUR DIGITAL PAYMENTS
           </h1>
           <p className="max-w-[600px]  mx-auto text-center mt-5">
-            {" "}
             Join us in shaping a future where every digital interaction
-            resonates with the
-        
-            essence of seamless financial empowerment.
-            {" "}
+            resonates with the essence of seamless financial empowerment.
           </p>
         </div>
-      
       </div>
       <div className="mt-6  max-w-screen mx-auto">
-        <video  autoPlay className="w-full"  loop muted>
+        <video autoPlay preload="auto" className="w-full" loop muted>
           <source src="paysaApp-video.mp4" type="video/mp4" />
           Your browser does not support the video format.
         </video>
