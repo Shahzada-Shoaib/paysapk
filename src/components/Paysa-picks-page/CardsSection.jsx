47e78d26270f5e720fc0b182e88b81d73f1c9cb9
Shahzada Salman <shahzada.salman@convertdigital.com.au>
2024-12-29 18:16:23 +0500

    adding fixes

--- src/components/Paysa-picks-page/CardsSection.jsx
+++ src/components/Paysa-picks-page/CardsSection.jsx
@@ -8,19 +8,21 @@ const CardsSection = () => {
   const items = [
     {
       title: "MEEZAN BANK COLLABORATION",
-      description: "Meezan Bank, Pakistan’s leading Islamic bank and PaySa have partnered to enable Point of Sale (POS) payment services to tap into the country’s booming digital payment ecosystem.",
+      description:
+        "Meezan Bank, Pakistan’s leading Islamic bank and PaySa have partnered to enable Point of Sale (POS) payment services to tap into the country’s booming digital payment ecosystem.",
       link: "https://www.meezanbank.com/meezan-bank-enters-into-a-strategic-collaboration-with-paysa/",
-      
     },
     {
       title: "BREAST CANCER AWARENESS!",
-      description: "Join the PaySa team to support survivors, honor the fighters, and remember those we’ve lost. As we Stand Strong for Breast Cancer Awareness",
-      link: "https://www.instagram.com/p/DBdtZuSML5X/?img_index=1"
+      description:
+        "Join the PaySa team to support survivors, honor the fighters, and remember those we’ve lost. As we Stand Strong for Breast Cancer Awareness",
+      link: "https://www.instagram.com/p/DBdtZuSML5X/?img_index=1",
     },
     {
       title: " RUNNING TO A BRIGHTER FUTURE",
-      description: "To raise $1 million for providing quality education to 7,000 children across 2,000+ schools.",
-      link: "https://www.instagram.com/p/DBYgaLCsdnY/"
+      description:
+        "To raise $1 million for providing quality education to 7,000 children across 2,000+ schools.",
+      link: "https://www.instagram.com/p/DBYgaLCsdnY/",
     },
   ];
 
@@ -49,16 +51,18 @@ const CardsSection = () => {
         {items.map((item, index) => (
           <div
             key={index}
-            className={`p-6 flex items-center justify-center rounded-lg transition-all duration-300 ${index === 1 ? "mt-0 w-[150%]" : "mt-10 w-[100%]"
-              }`}
+            className={`p-6 flex items-center justify-center rounded-lg transition-all duration-300 ${
+              index === 1 ? "mt-0 w-[150%]" : "mt-10 w-[100%]"
+            }`}
           >
             {/* Replace the image with AboutUsCard */}
             <a href={item.link} target="_blank" rel="noopener noreferrer">
               <AboutUsCard
                 title={item.title}
+                heightClass="h-[280px]"
                 description={item.description}
                 className="bg-white shadow-md"
-                isPrimary={index === 0 || index === 2} 
+                isPrimary={index === 0 || index === 2}
               />
             </a>
           </div>
