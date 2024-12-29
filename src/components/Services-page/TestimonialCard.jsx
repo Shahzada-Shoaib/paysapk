704c64c262c5bdb3da52427c5a20017831404d57
Shahzada Salman <shahzada.salman@convertdigital.com.au>
2024-12-29 18:09:34 +0500

    text justifucation fixes

--- src/components/Services-page/TestimonialCard.jsx
+++ src/components/Services-page/TestimonialCard.jsx
@@ -48,37 +48,29 @@
 
 // export default TestimonialCard;
 
-
 import React, { useState } from "react";
 
 function TestimonialCard({ brandLogo = "", testimonial }) {
   // const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div
-   
-      className="text-white"
-    >
+    <div className="text-white">
       <div className="bg-[#185C8333] w-[100%] md:w-[355px] h-[476px] rounded-3xl">
         <div className="flex flex-col h-full">
-          
-            
-           
-            <div className="text-white bg-[#185C8333] w-[100%] md:w-[355px] h-[476px] rounded-3xl">
-              <div className="flex flex-col h-full pl-10">
-                <div className="flex items-center justify-start flex-1 ">
-                  <img src={brandLogo} alt="brand testimonial" />
-                </div>
+          <div className="text-white bg-[#185C8333] w-[100%] md:w-[355px] h-[476px] rounded-3xl">
+            <div className="flex flex-col h-full pl-10">
+              <div className="flex items-center justify-start flex-1 ">
+                <img src={brandLogo} alt="brand testimonial" />
+              </div>
 
-                <div className="flex flex-col flex-1">
-                  <div className="mb-4 text-lg text-yellow-400">
-                    &#9733; &#9733; &#9733; &#9733; &#9733;
-                  </div>
-                  <p className="w-[240px] text-justify">{testimonial}</p>
+              <div className="flex flex-col flex-1">
+                <div className="mb-4 text-lg text-yellow-400">
+                  &#9733; &#9733; &#9733; &#9733; &#9733;
                 </div>
+                <p className="w-[240px]">{testimonial}</p>
               </div>
             </div>
-          
+          </div>
         </div>
       </div>
     </div>
@@ -86,5 +78,3 @@ function TestimonialCard({ brandLogo = "", testimonial }) {
 }
 
 export default TestimonialCard;
-
-
